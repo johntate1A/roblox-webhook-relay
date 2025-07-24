525c8dde1c0ab748094053086ecf4a7952fea521
johntate1A <asfgasfasfasf8@gmail.com>
2025-07-24 21:11:55 +0800

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
+app.post('/webhook', (req, res) => {
+  console.log('Received data:', req.body);
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
